8e4321e92504894b178d982d3823b0d70316c3c8
hjunior29 <helderjuniorsilvalima@gmail.com>
2023-09-27 01:40:02 -0300

    scraper properties prices

--- scrapers/zapimoveis.py
+++ scrapers/zapimoveis.py
@@ -1,26 +1,31 @@
 import requests
 from bs4 import BeautifulSoup
 import utils
+import json
+import logging
+from dotenv import load_dotenv
+import os
 
 def get_property_locations():
     
-    url = "https://www.zapimoveis.com.br/venda/imoveis/go+goiania/?__ab=exp-aa-test:control,new-discover-zap:alert,vas-officialstore-social:enabled&transacao=venda&onde=,Goi%C3%A1s,Goi%C3%A2nia,,,,,city,BR%3EGoias%3ENULL%3EGoiania,-16.686891,-49.264794,&pagina=1"
-    
     try:
-        response = requests.get(url, headers=utils.headers.get_basic_headers())
+        response = requests.get(os.getenv('URL_ZAPIMOVEIS'), headers=utils.headers.get_basic_headers())
         response.raise_for_status()
-        print(response)
     except requests.exceptions.HTTPError as e:
-        print(f"Error accessing URL:\n {e}")
+
+        logging.error(f"Error accessing URL: {e}")
         return []
 
     soup = BeautifulSoup(response.text, 'html.parser')
 
     address_tags = soup.find_all(class_='l-text l-u-color-neutral-28 l-text--variant-heading-small l-text--weight-medium card__address')
-    print(address_tags)
-
     addresses = [tag.get_text(strip=True) for tag in address_tags]
-    print(addresses)
 
-    return addresses
+    listing_price_divs = soup.find_all('div', class_='listing-price')
+    prices_tags = [div.find('p', class_='l-text l-u-color-neutral-28 l-text--variant-heading-small l-text--weight-bold undefined') for div in listing_price_divs]
+    prices = [int(tag.get_text(strip=True).replace("R$ ", "").replace(".", "")) for tag in prices_tags if tag]
+
+    utils.db_request.add_properties(utils.convert.list_to_dic(addresses, prices))
+
+    return
 
