e4a6d18be77f322f95a5782677961a768df1e3f6
hjunior29 <helderjuniorsilvalima@gmail.com>
2023-09-27 01:39:36 -0300

    addin content-type json header

--- utils/headers.py
+++ utils/headers.py
@@ -1,6 +1,6 @@
 def get_basic_headers():
-    
     headers = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
+    "Content-Type": "application/json"
     }
     return headers
