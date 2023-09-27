014273512af574dbb8d2eb42f50151b48b6f8b7d
hjunior29 <helderjuniorsilvalima@gmail.com>
2023-09-27 02:06:48 -0300

    refactor fun

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import logging
 def main():
 
     logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
-    scrapers.zapimoveis.get_property_locations()
+    scrapers.zapimoveis.get_properties_data()
 
 if __name__ == "__main__":
     main()
